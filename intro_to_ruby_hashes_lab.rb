5e754e5ac6a1becea6feb4c5402cc88ac4e528ec
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-20 00:05:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,7 @@ def my_hash_creator(key, value)
     hash = {
       :key => "value"
     }
-hash.fetch(key)
-hash.fetch(value)
+hash.fetch(key, value)
   # return a hash that includes the key and value parameters passed into this method
 end
 
