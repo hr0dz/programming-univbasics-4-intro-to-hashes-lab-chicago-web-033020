e5f1c02df71592747a17e198cf29de72383a9aeb
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-20 00:13:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-    hash = {
-      :key => "value"
-    }
-hash.fetch(key, value)
+    hash = Hash.new
+    hash[key]
+    
   # return a hash that includes the key and value parameters passed into this method
 end
 
